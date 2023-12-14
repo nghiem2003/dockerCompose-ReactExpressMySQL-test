61b3e2716a410737087991fad654e5ee7521f45f
Nghiem Duy <nghiem.eo.bua.18@gmail.com>
2023-12-14 15:10:36 +0700

    remove server folder

--- server/src/config/viewEngine.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-let configViewEngine = (app) => {
-    app.use(express.static("./src/public"));
-    app.set("view enginne", "ejs");
-    app.set("views", "./src/views")
-}
-
-module.exports = configViewEngine;
